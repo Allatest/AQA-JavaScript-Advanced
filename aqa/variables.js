5c93a5174fc129ecf8f82cf0dd176335109494dd
Allatest <work.alla.luts@gmail.com>
2023-09-23 17:37:17 +0200

    New changes

---
+++ aqa/variables.js
@@ -0,0 +1,17 @@
+var adminName = "Kate";
+var userName = "Black";
+
+console.log(typeof adminName); // string
+console.log(typeof userName);  // string
+
+var message = "awesome"
+
+var password1 = 1234;
+var password2 = 1111;
+console.log(`Javascript is: ${message}, ${password1}, ${password2}`);
+
+var Mynumber = "23";
+console.log(Mynumber); // "23"
+
+Mynumber = "12";
+console.log(Mynumber); // "12"

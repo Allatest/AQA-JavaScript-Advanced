9c6017ba98645dbc4b1828b5f272b6dff1359c34
Allatest <work.alla.luts@gmail.com>
2023-10-12 15:08:21 +0200

    Add fixed changes

--- js-operators-coercion/string.js
+++ js-operators-coercion/string.js
@@ -4,5 +4,8 @@ console.log(myString.length);
 let replacedString = myString.replace(/Wikipedia/g, "Book");
 console.log(replacedString);
 
+let containsWorld = myString.includes("world");
+console.log(containsWorld);
+
 let endsWithFoundation = myString.endsWith("Foundation.");
 console.log(endsWithFoundation);

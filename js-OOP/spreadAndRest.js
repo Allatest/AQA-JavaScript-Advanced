e0aec37b58194858190b147bc8a256a7a4cd2030
Allatest <work.alla.luts@gmail.com>
2023-10-16 19:15:42 +0200

    Your commit message here

---
+++ js-OOP/spreadAndRest.js
@@ -0,0 +1,17 @@
+// Task 1
+const var1 = [1, 2, 3];
+const var2 = [4, 5, 6];
+
+const mergedArray = [...var1, ...var2];
+
+console.log(mergedArray);
+
+// Task 2
+function multiply(multiplier, ...args) {
+    const result = args.map((value) => multiplier * value);
+    return result;
+  }
+  
+  const result = multiply(4, 9, 83, 3);
+  console.log(result); 
+  

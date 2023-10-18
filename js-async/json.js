8870ef9672d0d75845e2ef3a478093da65553d5d
Allatest <work.alla.luts@gmail.com>
2023-10-18 16:26:42 +0200

    Hometask8

---
+++ js-async/json.js
@@ -0,0 +1,15 @@
+// Task 1
+const userSettings = {
+    username: "john_doe",
+    theme: "dark",
+    language: "en",
+    notifications: true,
+};
+
+const userSettingsJSON = JSON.stringify(userSettings);
+console.log(userSettingsJSON);
+
+
+const deserializedUserSettings = JSON.parse(userSettingsJSON);
+console.log(deserializedUserSettings);
+
